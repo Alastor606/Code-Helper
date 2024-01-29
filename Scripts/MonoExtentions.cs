3dcec9e54fea6947f423da5b47aaf4b5dfac3c77
alastor <114815838+Alastor606@users.noreply.github.com>
2024-01-29 14:48:22 +0300

    Update

--- Scripts/MonoExtentions.cs
+++ Scripts/MonoExtentions.cs
@@ -6,22 +6,30 @@ namespace CodeHelper.Unity
 {
     public static class MonoExtentions
     {
-        public static void WaitAndDo(this MonoBehaviour self, float time, Action toDo)
+
+        /// <summary>Starts local coroutine, wait given time and invoke action </summary>
+        /// <param name="time">Delay</param>
+        public static void WaitAndDo(this MonoBehaviour self, float delay, Action toDo)
         {
             self.StartCoroutine(Waiter());
             IEnumerator Waiter()
             {
-                yield return new WaitForSeconds(time);
+                yield return new WaitForSeconds(delay);
                 toDo.Invoke();
             }
         }
         
-        public static GameObject Instantiate(this MonoBehaviour self, GameObject prefab, Vector3 position, Transform parent = null, Quaternion rotation = default) 
+        public static T Instantiate<T>(this MonoBehaviour self, T prefab, Vector3 position, Transform parent = null, Quaternion rotation = default) where T : UnityEngine.Object
         {
             var rot = rotation == default ? Quaternion.identity : rotation;
-            GameObject obj = GameObject.Instantiate(prefab,position,rot);
-            if (parent != null) obj.transform.parent = parent;
+            var obj = parent == null ? UnityEngine.Object.Instantiate(prefab, position, rot) : UnityEngine.Object.Instantiate(prefab, parent);
             return obj;
+        }
+
+        private static void SetParams<T>(ref GameObject obj, Vector3 pos, Quaternion rot)
+        {
+            obj.SetPosition(pos);
+            obj.transform.rotation = rot;
         }
     }
 }
